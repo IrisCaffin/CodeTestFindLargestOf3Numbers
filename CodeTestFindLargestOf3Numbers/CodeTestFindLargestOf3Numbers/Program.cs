edd7301961ee85c1aec774dfe1425096f84ab765
Iris Caffin <iriscaffin@gmail.com>
2016-04-23 10:25:20 -0700

    Find the largest of three numbers

---
+++ CodeTestFindLargestOf3Numbers/CodeTestFindLargestOf3Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestFindLargestOf3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLargestNumber(6, 3, 9));
+        }
+
+        // <summary>
+        // Find the largest of three numbers
+        // </summary>
+        // <param name="a">first parameter</param>
+        // <param name="b">second parameter</param>
+        // <param name="c">third parameter</param>
+        // <returns></returns>
+
+        public static long FindLargestNumber(long a, long b, long c)
+        {
+            // Assume that the first value is the biggest
+            long biggest = a;
+
+            // Check if b is the biggest
+            if (b > biggest)
+                biggest = b;
+
+            // check if c is the biggest
+            if (c > biggest)
+                biggest = c;
+
+            return biggest;
+
+            Console.ReadLine();
+        }   
+    }
+}
